1a8d78cecedea76fcc5f020256658bcd1211ff67
ayyappan97899 <ayyappan97899@gmail.com>
2024-07-03 15:29:34 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
 
 export default function Home() {
-  let hj1 = "";
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
